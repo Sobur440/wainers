4cc38335034b6b8627355f1fcab40895106b067f
Sobur Abubakar <abubakarsobur440@gmail.com>
2024-03-13 17:44:49 +0100

    update

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,17 +1,56 @@
-import { useEffect, useState } from "react";
 import Products from "../components/Products";
 
 function Shop() {
-  const [goods, setGoods] = useState([]);
-
-  const url = "http://localhost:8000/goods";
-
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setGoods(data))
-      .catch(() => console.log("their was an error"));
-  }, []);
+  const goods = [
+    {
+      name: "Nice Shoe",
+      price: "$20",
+      image: "shoes.png",
+      id: 1,
+    },
+    {
+      name: "Nice Bangles",
+      price: "$350",
+      image: "bangles.png",
+      id: 2,
+    },
+    {
+      name: "Nice Chains",
+      price: "$72",
+      image: "chains.png",
+      id: 3,
+    },
+    {
+      name: "Nice Socks",
+      price: "$23",
+      image: "halfsocks.jpg",
+      id: 4,
+    },
+    {
+      name: "Nice Perfume",
+      price: "$45",
+      image: "perfume.png",
+      id: 5,
+    },
+    {
+      name: "Nice Rings",
+      price: "$20",
+      image: "tenrings.jpg",
+      id: 6,
+    },
+    {
+      name: "Nice Towel",
+      price: "$15",
+      image: "towels.jpg",
+      id: 7,
+    },
+    {
+      name: "Nice Watch",
+      price: "$600, 000",
+      image: "watch.jpg",
+      id: 8,
+    },
+  ];
 
   if (goods.length <= 0)
     return (
