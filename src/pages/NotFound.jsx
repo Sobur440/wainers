fd2834c3e28d8998ca0348c9304662108ec51a9a
Sobur Abubakar <abubakarsobur440@gmail.com>
2024-03-13 18:03:34 +0100

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
 function NotFound() {
-  return <div>Page not Found</div>;
+  return (
+    <div className="w-[100%] h-[100vh] fixed flex flex-col justify-center items-center">
+      <div>
+        <img
+          src="/no-results.png"
+          alt="NO result"
+          className="h-[30vh] w-310vw] "
+        />
+      </div>
+      <p className="text-[4vw]">Page not found</p>
+    </div>
+  );
 }
 
 export default NotFound;
