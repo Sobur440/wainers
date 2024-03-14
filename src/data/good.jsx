6a841877213d521f44e7b4d537706e15ca001e49
Sobur Abubakar <abubakarsobur440@gmail.com>
2024-03-14 09:38:07 +0100

    moved the images in a separate folder for more cleaner code

---
+++ src/data/good.jsx
@@ -0,0 +1,50 @@
+export const goods = [
+  {
+    name: "Nice Shoe",
+    price: "$20",
+    image: "shoes.jpg",
+    id: 1,
+  },
+  {
+    name: "Nice Bangles",
+    price: "$350",
+    image: "bangles.jpg",
+    id: 2,
+  },
+  {
+    name: "Nice Chains",
+    price: "$72",
+    image: "chains.jpg",
+    id: 3,
+  },
+  {
+    name: "Nice Socks",
+    price: "$23",
+    image: "halfsocks.jpg",
+    id: 4,
+  },
+  {
+    name: "Nice Perfume",
+    price: "$45",
+    image: "perfume.png",
+    id: 5,
+  },
+  {
+    name: "Nice Ring",
+    price: "$20",
+    image: "rings.jpg",
+    id: 6,
+  },
+  {
+    name: "Nice Towel",
+    price: "$15",
+    image: "towels.jpg",
+    id: 7,
+  },
+  {
+    name: "Nice Watch",
+    price: "$600, 000",
+    image: "watch.jpg",
+    id: 8,
+  },
+];
